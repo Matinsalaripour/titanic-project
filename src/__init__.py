3e939ec65c09586658b864303ce590cd74d105f0
Matinsalaripour84 <matinsalaripour84@gmail.com>
2026-06-24 11:11:33 +0330

    Change libraries in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 # src/__init__.py
 
 from .data_loader import load_train_data, load_test_data
-from .features import clean_and_engineer, create_features
+from .features import TitanicFeatureEngineer
